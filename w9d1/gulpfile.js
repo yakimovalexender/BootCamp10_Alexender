538e9701e48d3c1fea02ce8705862a1b3104c029
yakimovalexender <yakimovalexender@gmail.com>
2019-03-28 17:01:08 +0200

    added new files

---
+++ w9d1/gulpfile.js
@@ -0,0 +1,15 @@
+
+const gulp = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+const rigger = require('gulp-rigger');
+
+gulp.task('html', () =>{
+    gulp.src('./src/*.html')
+.pipe(rigger())
+.pipe(
+    htmlmin({
+collapseWhitespace: true,
+})
+)
+.pipe(gulp.dest('./build/'))
+});
