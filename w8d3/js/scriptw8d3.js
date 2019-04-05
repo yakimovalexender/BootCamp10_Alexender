23ecaa677a56c69682aee099f9d3470644acebe2
yakimovalexender <yakimovalexender@gmail.com>
2019-04-05 19:45:28 +0300

    added HW

--- w8d3/js/scriptw8d3.js
+++ w8d3/js/scriptw8d3.js
@@ -45,23 +45,23 @@
 //      seconds = '0' + seconds;
 //     }
 //     let milisec = parseInt((date.getMilliseconds()) / 100);
-    
+
 //      return `${minutes} : ${seconds}. ${milisec}`;
 //     }
 //     console.log(getFormattedTime(Date.now()))
 //     console.log(
 //      getFormattedTime(1523621052858)
 //     ); // 04:12.8
-    
+
 //     console.log(
 //      getFormattedTime(1523621161159)
 //     ); // 06:01.1
-    
+
 //     console.log(
 //      getFormattedTime(1523621244239)
 //     ); // 07:24.2
 
-    // ===============end=======
+// ===============end=======
 
 
 //    module-12-task-02
@@ -89,7 +89,7 @@
 //     console.log(rand);    
 //   }, 1000);
 //   start.disabled = true;
-  
+
 // };
 
 //   function stopColor(){
@@ -139,19 +139,19 @@ const timer = {
 */
 
 /*
-* Обновляет поле счетчика новым значением при вызове
-* аргумент time это кол-во миллисекунд
-*/
+ * Обновляет поле счетчика новым значением при вызове
+ * аргумент time это кол-во миллисекунд
+ */
 // function updateClockface(elem, time) {
-  // Используйте функцию getFormattedTime из задания #1
-  // elem.textContent = getFormattedTime(time);
+// Используйте функцию getFormattedTime из задания #1
+// elem.textContent = getFormattedTime(time);
 // };
 
 // function
 
 /*
-* Подсветка активной кнопки
-*/
+ * Подсветка активной кнопки
+ */
 /*
 function setActiveBtn(target) {
   if(target.classList.contains('active')) {
@@ -186,7 +186,7 @@ function setActiveBtn(target) {
  * 
  * Если же пользователь ввел не число, то промис выполняется с ошибкой
  * и значением "Некорректный ввод!".
-*/
+ */
 
 // const DELAY = 1000;
 // const quantity = 100;
@@ -300,7 +300,7 @@ function setActiveBtn(target) {
 
 // for (let i=0; i < arr.length; i++) {
 //     console.log(arr[i]);
-    
+
 // }
 
 // console.log(arr);
@@ -363,7 +363,7 @@ function setActiveBtn(target) {
 //           temperatureDegree.textContent = temperature;
 //           temperatureDescription.textContent = summary;
 //         })
-      
+
 //     })
 //   }  
 // }
@@ -387,7 +387,7 @@ function setActiveBtn(target) {
 //      continue
 //     }
 //   console.log(i);
-  
+
 // }
 
 
@@ -400,7 +400,7 @@ function setActiveBtn(target) {
 
 // fruits.forEach(function(item, index, array) {
 //   console.log(item , index,);
-  
+
 // });
 
 // let newLength = fruits.push('Апельсин');
@@ -510,7 +510,7 @@ function setActiveBtn(target) {
     показывать alert со сообщением 'Доступ в секретный бункер разрешен!'
     
   - Если что-то другое — показывать alert с сообщением 'Активирована система защиты!'
-*/ 
+*/
 
 // const correctPassword = '123';
 // const enterMessage = prompt('Введите пароль доступа');
@@ -529,3 +529,253 @@ function setActiveBtn(target) {
 // alert('Активирована система защиты!');
 // };
 
+/*
+  Необходимо написать скрипт проверки количества товаров на складе.
+  Есть переменные total (количество товаров на складе) и ordered (единиц товара в заказе).
+  
+  Сравни эти значения и по результатам выведи:
+  
+    - Если в заказе указано число, превышающее количество товаров на складе, то выведи сообщение "На складе недостаточно твоаров!"
+    - Если в заказе указано число товаров, равное количеству товара на складе, то выведи сообщение "Вы забираете весь товар cо склада!"
+    - В иных случаях выводи сообщение "Заказ оформлен, с вами свяжется менеджер"
+    
+  Проверь работоспособность кода с разными значениями переменной ordered.
+*/
+
+// const total = 100;
+// const ordered = +prompt('Enter quantity goods');
+// // console.log(ordered,typeof(ordered));
+// // console.log(typeof(total));
+
+// switch (true) {
+//   case (ordered == total):
+//     alert("Вы забираете весь товар cо склада!")
+//     console.log(ordered);
+//     break;
+
+
+//   case (ordered > total):
+//     alert("На складе недостаточно товаров!")
+//     break;
+
+
+//   default:
+//     alert("Заказ оформлен, с вами свяжется менеджер")
+// };
+
+// v2
+
+// const total = 100;
+// const ordered = +prompt('Enter quantity goods');
+
+// if ( ordered == total) {
+//   alert("Вы забираете весь товар cо склада!");
+// }
+// if (ordered > total) {
+//   alert("На складе недостаточно товаров!");
+// }
+// if (ordered == null) {
+//   alert('You need entrer quantity goods!!!');
+// }
+// if (ordered < total) {
+// alert("Заказ оформлен, с вами свяжется менеджер");
+// };
+
+/*
+  Напиши скрипт который: 
+ 
+  - Через prompt cпрашивает 'Введите произвольное целое число'
+  - Если пользователь нажал Cancel - показывать alert 'Ну и ладно, пока!'
+  - Если посетитель вводит целое число - показывать alert со строкой 'Спасибо!'
+  - Если посетитель вводит что либо другое — показывать alert 'Необходимо было ввести целое число!'
+*/ 
+
+// let question = + prompt('Enter any whole number')
+// console.log(question,typeof(question));
+
+// if (question == 0) {
+//   alert('so... OK .... bye-bye !!!');
+// }
+// if (parseInt(question) != question) {
+//   alert('Its not whole number !!!');
+// }
+// if (parseInt(question) ==question) {
+//   alert ('Thanks!');
+// };
+
+/* 
+  В переменную value записывается случайное число.
+  
+  Объяви переменную type, в которую, используя ветвления запиши строку:  
+    - "even" если value четное
+    - "odd" если value не четное
+
+  PS: попробуй использовать тернарный оператор
+*/
+
+// const value = Number.parseInt(Math.random() * 100);
+// console.log(value);
+// let type=value%2 == 0 ? "even" : "odd";
+// // even= (value%2);
+
+// console.log(`${value} is ${type}`);
+
+/* 
+  Создай скрипт поиска отелей, где пользователь 
+  с помощью prompt должен ввести число от 1 до 5
+  
+  Проверить что пользователь ввел именно цифру от 1 до 5
+  
+  Если пользователь нажал Cancel, то выведи alert с текстом 'Очень жаль, приходите еще!'
+  
+  Если было введено что либо кроме чисел 1-5, вывести alert с текстом 'Неверный ввод, возможные варианты 1-5!'
+  
+  Если же пользовател ввел валидное число, используя switch, вывести alert с одной из строк:
+  
+    1 - "Каталог хостелов" 
+    2 - "Каталог бюджетных отелей"
+    3 - "Каталог отелей ***"
+    4 - "Каталог отелей ****"
+    5 - "Каталог лучших отелей"
+*/
+// let select = +prompt('Enter number 1-5');
+// console.log(select);
+// switch(select) {
+//   case 0 :
+  
+  
+//   alert('Очень жаль, приходите еще!');
+//   break;
+
+//   case 1 :
+//   alert('Каталог хостелов');
+//   break;
+
+//   case 2 :
+//   alert('Каталог бюджетных отелей');
+//   break;
+
+//   case 3 :
+//   alert('Каталог отелей ***');
+//   break;
+
+//   case 4 :
+//   alert('Каталог отелей ****');
+//   break;
+  
+//   case 5 :
+//   alert('Каталог fine отелей');
+//   break;
+
+//   default :
+//   alert("Неверный ввод, возможные варианты 1-5!")
+// };
+/*
+1. Задача 1
+
+Напиши скрипт имитирующий авторизацию администратора в панели управления.
+
+Есть переменная message в которую будет записано сообщение о результате. При загрузке страницы у посетителя запрашивается пароль через prompt:
+
+    Если нажали Cancel, записать в message строку 'Отменено пользователем!'
+    В протовном случае, если введен пароль который совпадает со значением константы ADMIN_PASSWORD, записать в message строку 'Добро пожаловать!'
+    В противном случае, то есть если ни одно из предыдущих условий не выполнилось, записать в message строку 'Доступ запрещен, неверный пароль!'
+    После всех проверок вывести в alert значение переменной message.
+
+const ADMIN_PASSWORD = 'm4ng0h4ckz';
+let message;
+*/
+
+// const ADMIN_PASSWORD = 'm4ng0h4ckz';
+// let question = prompt('Enter password')
+// let message = question;
+// if (message == null) {
+//   alert('Отменено пользователем!');
+// };
+// if(message==ADMIN_PASSWORD) {
+//   alert('Добро пожаловать!');
+// };
+
+// if(message !=ADMIN_PASSWORD) {
+//   alert('Доступ запрещен, неверный пароль!');
+// };
+
+
+
+// alert(question);
+
+/*
+На счету пользователя есть 23580 кредитов, значение хранится в переменной credits (создай и присвой). Пользователь решает купить ремонтных дроидов, которые стоят по 3000 кредитов за штуку. Цена одного дроида хранится в переменной pricePerDroid (создай и присвой).
+
+При посещении страницы, используя prompt, необходимо спросить количество дроидов которые пользователь хочет купить и сохранить в переменную.
+
+Напиши скрипт который:
+
+    Если в prompt была нажата Cancel, выводит в консоль сообщение 'Отменено пользователем!'.
+    В противном случае, рассчитывает общую цену заказа и сохраняет в переменной totalPrice.
+    Проверяет сможет ли пользователь оплатить заказ:
+        если сумма к оплате превышает количество кредитов на счету, выводи в консоль сообщение 'Недостаточно средств на счету!'.
+        в противном случае необходимо посчитать остаток кредитов на счету и вывести сообщение 'Вы купили [число] дроидов, на счету осталось [число] кредитов.'.
+*/
+
+/*
+let credits = 23580;
+let pricePerDroid = 3000;
+let question = +prompt('How many droids do you want to buy?');
+let totalPrice = (question * pricePerDroid);
+console.log( totalPrice);
+
+let totalMoney = (credits - totalPrice);
+if(question == 0) {
+  alert('Cansel user');
+};
+
+if( totalPrice < credits) {
+  alert("you buy " + (question) + " droids, you have " + (totalMoney));
+};
+
+if( totalPrice > credits) {
+  alert('you have NO money! You have only ' + credits + ' credits');
+}
+*/
+
+/*
+Пользователь может оформить доставку товара к себе в страну, указав ее при посещении страницы в prompt. Учти, что пользователь может ввести имя страны не только буквами нижнего регистра, а к примеру 'кИтАЙ'.
+
+Напиши скрипт который выводит сообщение о стоимости доставки в указанную страну. Формат сообщения: 'Доставка в [страна] будет стоить [цена] кредитов'.
+
+Но доставка есть не везде, если указанной страны нет в списке, то выводи в консоль сообщение 'В вашей стране доставка не доступна'.
+
+Ниже приведен список стран и стоимость доставки.
+
+    китай - 100 кредитов
+    южная америка - 250 кредитов
+    австралия - 170 кредитов
+    индия - 80 кредитов
+    ямайка - 120 кредитов
+
+PS: используй switch
+*/
+
+
+let country = prompt('Enter country from which you want to arrange delivery ');
+let countryRegister = country.toLowerCase();
+console.log( countryRegister);
+
+switch (countryRegister) {
+  case 'китай' :
+  alert('100 кредитов');
+  break;
+  case 'южная америка':
+  alert('250 кредитов');
+  break;
+  case 'австралия':
+  alert('170 кредитов');
+  break;
+  case 'индия':
+  alert('80 кредитов');
+  break;
+  case 'ямайка':
+  alert('120 кредитов');
+  break;
+}
