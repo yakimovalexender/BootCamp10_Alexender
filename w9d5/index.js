64a9e1729f0491769275c278be318fcd61b8cb81
yakimovalexender <yakimovalexender@gmail.com>
2019-04-18 17:04:22 +0300

    added refreshfs

--- w9d5/index.js
+++ w9d5/index.js
@@ -1,18 +1,42 @@
 
+
+
+function alertValue(elementParametr) {
+    console.log(elementParametr.value);
+    console.log(elementParametr.className);
+}
+
+
 let firstNameId = 'first-name';
 let firstNameEl = document.getElementById(firstNameId);
+// console.log(firstNameEl.value);
+// console.log(firstNameEl.className);
+alertValue(firstNameEl);
+
 
 let lastNameId = 'last-name';
 let lastNameEl = document.getElementById(lastNameId);
+// console.log(lastNameEl.value);
+// console.log(lastNameEl.className);
+alertValue(lastNameEl);
+
 
 let adressId = 'adress';
 let adressEl = document.getElementById(adressId);
+// console.log(adressEl.value);
+// console.log(adressEl.className);
+alertValue(adressEl);
 
 let citiesId = 'cities';
 let citiesEl = document.getElementById(citiesId);
+alertValue(citiesEl);
 
 let hobbiesId = 'hobbies';
 let hobbiesEl = document.getElementById(hobbiesId);
+// console.log(hobbiesEl.value);
+// console.log(hobbiesEl.className);
+alertValue(hobbiesEl);
+
 
 let avatarWrapperId = 'avatar-wrapper';
 let avatarWrapperEl = document.getElementById(avatarWrapperId);
@@ -20,11 +44,38 @@ let avatarWrapperEl = document.getElementById(avatarWrapperId);
 let avatarId = 'avatar';
 let avatarEl = document.getElementById(avatarId);
 
-firstNameEl.value = 'Andrey';
-lastNameEl.className = 'last-name-input default-input error-input';
-avatarEl.src='https://icdn.lenta.ru/images/2019/04/13/00/20190413005744310/pic_6c41035026b5a1cbe8fc4eef2af7b6a4.jpg'
-// lastNameEl.setAttribute('value', 'kamasutra');
+// function alertValue() {
+//     console.log(firstNameEl.value);
+//     console.log(lastNameEl.value);
+//     console.log(adressEl.value);
+//     console.log(citiesEl.value);
 
-avatarEl.title='im aWatar!';
-window.alert(lastNameEl.className);
-lastNameEl.title='wrong last name'
+// }
+
+
+
+// alertValue();
+
+
+// firstNameEl.value = 'Andrey';
+// // console.log(firstNameEl.value);
+// // console.log(firstNameEl.className);
+// alertValue(firstNameEl);
+
+
+
+lastNameEl.className = 'last-name-input default-input error-input';
+avatarEl.src = 'https://icdn.lenta.ru/images/2019/04/13/00/20190413005744310/pic_6c41035026b5a1cbe8fc4eef2af7b6a4.jpg'
+// lastNameEl.setAttribute('value', 'kamasutra');
+// console.log(lastNameEl.value);
+// console.log(lastNameEl.className);
+
+
+avatarEl.title = 'im aWatar!';
+// console.log(lastNameEl.className);
+// alertValue(avatarEl)
+
+lastNameEl.title = 'wrong last name'
+// console.log(lastNameEl.value);
+// console.log(lastNameEl.className);
+// alertValue(lastNameId);
